2066619b0c5170a98c036f6e5776b51d7549e454
Peterkoricar <korkosk123@gmail.com>
2020-09-24 08:25:58 +0200

    cvicenie 2

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -2,7 +2,21 @@ package com.appslab;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+    public static void main(String[] args)
+    {
+        System.out.println(numberEqual(1,8,1));
+    }
+    public static int numberEqual(int a, int b, int c)
+    {
+        int equal = 0;
+        if (a == b)
+            equal++;
+        if (a == c)
+            equal++;
+        if(b == c)
+            equal++;
+        if (equal == 1)
+            equal = 2;
+        return equal;
     }
 }
