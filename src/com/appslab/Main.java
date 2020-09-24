bea71ac2c095db6e8ed12f102c0586f5c78a10ce
Peterkoricar <korkosk123@gmail.com>
2020-09-24 08:15:43 +0200

    cvic 01

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,10 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        System.out.println(totalDistance(0.50, 5.00, 50.00));
+    }
+
+    public static double totalDistance(double heigh, double length, double towerheigh) {
+        return ((towerheigh / heigh) * (length + heigh));
     }
 }
