1d783ce06161bb377cb36fac8ec9555f15120669
Peterkoricar <korkosk123@gmail.com>
2020-09-29 08:29:39 +0200

    cvic09

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,20 +1,27 @@
 package com.appslab;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sum());
+        System.out.println(oddEven());
     }
 
-    public static int sum() {
-        var rand = new Random();
-        var arList = new ArrayList<Integer>();
-        for (int i = 0; i < 10; i++)
-            arList.add(rand.nextInt());
-        System.out.println(Collections.max(arList).byteValue());
-        System.out.println(Collections.min(arList).byteValue());
-        return (Collections.max(arList).byteValue() - Collections.min(arList).byteValue());
+    public static int oddEven() {
+        var odds = new ArrayList<Integer>();
+        var even = new ArrayList<Integer>();
+        int number = 0;
+        int oddsC = 0;
+        int evenC = 0;
+        for (int i = 0; i < 5; i++) {
+            number++;
+            if (number % 2 == 0) {
+                even.add(number);
+                evenC += number;
+            } else {
+                odds.add(number);
+                oddsC += number;
+            }
+        }
+        return evenC - oddsC;
     }
 }
