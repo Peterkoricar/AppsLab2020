8035f28ad3a40a18a2c56b68fa9c130575aaf250
Peterkoricar <korkosk123@gmail.com>
2020-09-29 07:36:38 +0200

    cvic03

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,12 @@
 package com.appslab;
 
-public class Main {
 
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        System.out.println(isTriangle(12,1,1));
+    }
+    public static Boolean isTriangle(int a, int b, int c){
+        return a + b > c && a + c > b && b + c > a;
     }
 }
