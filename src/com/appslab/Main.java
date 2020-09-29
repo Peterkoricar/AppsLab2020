294281ec686a9ded45ac9e39eb6cb1a079e44425
Peterkoricar <korkosk123@gmail.com>
2020-09-29 07:38:26 +0200

    cvic04

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,12 +1,18 @@
 package com.appslab;
-
-
-
+import java.util.ArrayList;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isTriangle(12,1,1));
+        System.out.println(Multiply(9));
     }
-    public static Boolean isTriangle(int a, int b, int c){
-        return a + b > c && a + c > b && b + c > a;
+
+    public static int Multiply(int a) {
+        var numbers = new ArrayList<Integer>();
+        var rand = new Random();
+        numbers.add(rand.nextInt(100));
+        int b = numbers.size();
+        for(int i = 0; i < b; i++)
+            a = numbers.get(i);
+        return a * b;
     }
 }
