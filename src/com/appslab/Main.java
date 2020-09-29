18ebe052ad38c7d2ed394fc416056d22d97ef487
Peterkoricar <korkosk123@gmail.com>
2020-09-29 07:40:43 +0200

    cvic05

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,18 +1,11 @@
 package com.appslab;
-import java.util.ArrayList;
-import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Multiply(9));
+        System.out.println(Slices(10,5,3));
     }
-
-    public static int Multiply(int a) {
-        var numbers = new ArrayList<Integer>();
-        var rand = new Random();
-        numbers.add(rand.nextInt(100));
-        int b = numbers.size();
-        for(int i = 0; i < b; i++)
-            a = numbers.get(i);
-        return a * b;
+    public static boolean Slices(int slices, int people, int pSlices)
+    {
+        return slices >= people * pSlices;
     }
 }
