9e1d16813d17f494a765fbe3473c7f565b6162a3
Peterkoricar <korkosk123@gmail.com>
2020-09-29 09:21:25 +0200

    cvic 10

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -2,7 +2,23 @@ package com.appslab;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+    public static void main(String[] args)
+    {
+        System.out.println(endings("mama", "ma"));
+    }
+    public static Boolean endings(String a , String b)
+    {
+        int t = 0;
+        int z = 0;
+        for (int i = a.length() -1; i > 0; i--)
+        {
+            for(int n = b.length() - 1; n > 0; n--)
+            {
+                t = i;
+                z = n;
+                return a.charAt(i) == b.charAt(n);
+            }
+        }
+        return  a.charAt(t) == b.charAt(z);
     }
 }
