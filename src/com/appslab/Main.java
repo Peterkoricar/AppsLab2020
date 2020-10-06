616469813ade6114f4ffd0dd892114de51576aa5
Peterkoricar <korkosk123@gmail.com>
2020-10-06 07:34:16 +0200

    cvic 12a

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -2,7 +2,10 @@ package com.appslab;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+    public static void main(String[] args)
+    {
+        Person student = new Person("Adam", 169454);
+        System.out.println(student.toString());
     }
+
 }
