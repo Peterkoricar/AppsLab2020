2ada444371cea85d2fc0e7e2e922b10a251372a2
Peterkoricar <korkosk123@gmail.com>
2020-10-06 07:43:21 +0200

    cvic 12a

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Person student = new Person("Adam", 169454);
+        Student student = new Student("Adam", 16541564);
         System.out.println(student.toString());
     }
-
 }
