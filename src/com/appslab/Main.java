0e67450689559381347c10403109cb3572fe30a1
Peterkoricar <korkosk123@gmail.com>
2020-10-06 07:52:29 +0200

    cvic 12b

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Student student = new Student("Adam", 16541564);
+        Student student = new Student("Adam" , 16541564, " 0944495544"," Cadca, Palarikova 559" );
         System.out.println(student.toString());
     }
 }
--- src/com/appslab/Student.java
+++ src/com/appslab/Student.java
@@ -5,15 +5,20 @@ public class Student
 {
     private String name;
     private  int number;
-
+    private String phoneNumber;
+    private String adress;
 
     @Override
     public String toString() {
-        return "Main{" +
+        return "Student{" +
                 "name='" + name + '\'' +
                 ", number=" + number +
+                ", PhoneNumber=" + phoneNumber +
+                ", Adress=" + adress +
                 '}';
     }
+
+
 
     public String getName() {
         return name;
@@ -31,13 +36,31 @@ public class Student
         this.number = number;
     }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
     public Student() {
 
     }
 
-    public Student(String name, int number)
+    public Student(String name, int number, String phoneNumber, String adress)
     {
         this.name = name;
         this.number = number;
+        this.phoneNumber = phoneNumber;
+        this.adress = adress;
     }
 }
