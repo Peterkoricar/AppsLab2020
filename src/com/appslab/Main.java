24ac76169fbd0c57fd2d00d877100a5853b22e60
Peterkoricar <korkosk123@gmail.com>
2020-10-26 18:35:05 +0100

    BE...1,2 uloha

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,39 +1,38 @@
 package com.appslab;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Random;
 
 
 public class Main {
 
     public static void main(String[] args)
     {
-
-        System.out.println(rooks());
-    }
-    public static Boolean rooks()
-    {
-        var numbers = new ArrayList<Integer>();
-        String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H"};
-        for (int i = 0; i < letters.length - 1; i++)
-            numbers.add(i);
-
-        var rand = new Random();
-        int a = rand.nextInt(letters.length - 1);
-        int b = rand.nextInt(letters.length - 1);
-
-        var number1 = numbers.get(a);
-        var number2 = numbers.get(b);
-
-        while (letters[a] == letters[b] && number1 == number2)
-        {
-            a = rand.nextInt(letters.length - 1);
+        var a = new ArrayList<Integer>();
+        for(int i = 0; i < 2; i++) {
+            a.add(1);
+            a.add(2);
         }
-        String r1 = letters[a] + number2;
-        String r2 = letters[b] + number1;
-
-        System.out.println(r1);
-        System.out.println(r2);
-        return (a == b|| number1.equals(number2));
+        a.add(1);
+        a.add(3);
+        a.add(4);
+        a.add(5);
+        AL(a);
+        addIf(1,a);
+    }
+    public static void AL (ArrayList<Integer> returnik ){
+        if(returnik.isEmpty())
+            System.out.println("List is empty");
+        else
+            returnik.forEach(n -> System.out.println(n));
+    }
+    public static void addIf(int f, ArrayList<Integer> returnik){
+        System.out.println("You are trying to add number " + f + " to the list.");
+        if(returnik.contains(f))
+            System.out.println("Oops, list already contains " + f);
+        else {
+            returnik.add(f);
+            returnik.forEach(n -> System.out.println("You had successfully added number " + n + "to the list"));
+        }
     }
 }
