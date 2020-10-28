d43d82e6892b3b4178b5644c5fc555c14bbd8491
Peterkoricar <korkosk123@gmail.com>
2020-10-28 10:31:42 +0100

    BE...1,2,3 uloha

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -6,10 +6,9 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         var a = new ArrayList<Integer>();
-        for(int i = 0; i < 2; i++) {
+        for (int i = 0; i < 2; i++) {
             a.add(1);
             a.add(2);
         }
@@ -18,21 +17,32 @@ public class Main {
         a.add(4);
         a.add(5);
         AL(a);
-        addIf(1,a);
+        addIf(6, a);
+        minusEven(a);
     }
-    public static void AL (ArrayList<Integer> returnik ){
-        if(returnik.isEmpty())
-            System.out.println("List is empty");
+
+    public static void AL(ArrayList<Integer> returnik) {
+        if (returnik.isEmpty())
+            System.out.println("List is empty.");
         else
-            returnik.forEach(n -> System.out.println(n));
+            returnik.forEach(n -> System.out.print(n + " "));
     }
-    public static void addIf(int f, ArrayList<Integer> returnik){
+
+    public static void addIf(int f, ArrayList<Integer> returnik) {
+        System.out.println(); //aby písalo na ďalší riadok
         System.out.println("You are trying to add number " + f + " to the list.");
-        if(returnik.contains(f))
+        if (returnik.contains(f))
             System.out.println("Oops, list already contains " + f);
         else {
             returnik.add(f);
-            returnik.forEach(n -> System.out.println("You had successfully added number " + n + "to the list"));
+            System.out.println("You had successfully added number " + f + "to the list.");
         }
+    }
+
+    public static void minusEven(ArrayList<Integer> returnik)
+    {
+        System.out.println("You are trying to remove even numbers from the list");
+            returnik.removeIf(n-> n%2 == 0);
+            System.out.println("New form of the list is " + returnik + ".");
     }
 }
